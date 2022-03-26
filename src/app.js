013918adabc01d8a833b901c386ae20b985e4fca
Boris Belmar <borisbelmarm@gmail.com>
2022-03-26 00:25:25 -0300

    Fix handlebars layout and body parser

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
 
